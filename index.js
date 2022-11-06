a99130718ac35995e8b60f3f7aa43171c4fa974a
Amiran <chicobes1@gmail.com>
2022-11-07 00:28:14 +0300

    correcrted bot

--- index.js
+++ index.js
@@ -1,35 +1,70 @@
 // когда пользователь пишет слово "рубай" в чате, бот отвечает "привет"
-const { Telegraf } = require('telegraf');
+const {Telegraf} = require('telegraf');
 const bot = new Telegraf('5609369539:AAFVT7jURIg_gpFTAQA5kZ8rZ6qlSz8aGbk');
 bot.start((ctx) => ctx.reply('Привет!'));
-// bot.command('pushme', (ctx) => {
-// 	// отправить стикер в чат
-// 	console.log(ctx.message);
-// })
-bot.on("message", (ctx) => {
-	if (includesRub(ctx.message.text || "")) {
-		ctx.reply(`https://picsum.photos/id/${randomInteger(1, 300)}/400/500`);
-	}
-});
-bot.on('message', (ctx) => {
-	if (ctx.message.voice.duration > 15) {
-		ctx.reply("ну пиздец еще вот это слушать");
+
+bot.on('sticker', (ctx) =>
+{
+	if (ctx.message.sticker.emoji == '👍')
+	{
+		ctx.reply('👌');
 	}
 });
 
-bot.on('message', (ctx) => {
-	if(ctx.message.sticker) {
-		ctx.reply('Стикеры не приветствуются');
+bot.on('voice', (ctx) =>
+{
+	if (ctx.message.voice.duration > 15)
+	{
+		ctx.reply("за такие длинные голосовые будешь гореть в аду");
 	}
 });
-function randomInteger(min, max) {
+bot.on('message', (ctx) =>
+{
+	hendler(ctx);
+})
+
+async function hendler(ctx)
+{
+	if (includesStrigoi(ctx.message.text))
+	{
+		await ctx.reply('))))');
+	}
+	else if (includesRub(ctx.message.text || ""))
+	{
+		await ctx.reply(`https://picsum.photos/id/${randomInteger(1, 300)}/400/500`);
+	}
+}
+
+function randomInteger(min, max)
+{
 	let rand = min + Math.random() * (max + 1 - min);
 	return Math.floor(rand);
 }
+
 // искать в сообщении слово "руб" убирать пробелы и приводить к нижнему регистру
-function includesRub(text) {
+function includesRub(text)
+{
 	return text.toLowerCase().replace(/\s/g, "").includes("руб");
 }
+
+// искать в сообщения слово которое может совпадать с любым словом из массива strigoi убирать пробелы повтрряющиеся буквы и приводить к нижнему регистру
+function includesStrigoi(text)
+{
+	let strigoi = ['strigoi', 'стригой', 'стриг', 'strig', 'стригой']
+	
+	let strigoiText = text.toLowerCase().replace(/\s/g, "").replace(/(.)\1+/g, "$1");
+	
+	for (let i = 0; i < strigoi.length; i++)
+	{
+		if (strigoiText.includes(strigoi[i]))
+		{
+			return true;
+		}
+	}
+	
+	return false;
+}
+
 bot.launch();
 
 
