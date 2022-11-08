39902c538afb1a99f7f92dba48e1beb4ec00335b
Amiran <chicobes1@gmail.com>
2022-11-08 18:31:06 +0300

    upgraid bot

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // когда пользователь пишет слово "рубай" в чате, бот отвечает "привет"
 const {Telegraf} = require('telegraf');
-const bot = new Telegraf('5349322670:AAGgL_PuGYb8T6CHxy_ZqY4BlMOnn8WaC64');
+const bot = new Telegraf('5609369539:AAFVT7jURIg_gpFTAQA5kZ8rZ6qlSz8aGbk');
 // 5349322670:AAGgL_PuGYb8T6CHxy_ZqY4BlMOnn8WaC64
 //5609369539:AAFVT7jURIg_gpFTAQA5kZ8rZ6qlSz8aGbk - тестовый бот
 bot.start((ctx) => ctx.reply('Привет!'));
@@ -30,16 +30,19 @@ async function hendler(ctx)
 {
 	if (includesStrigoi(ctx.message.text))
 	{
-		await ctx.telegram.sendPhoto(ctx.message.chat.id, {source: 'strigoi.jpg'});
+		await ctx.telegram.sendPhoto(ctx.message.chat.id, {source: 'strigoi.jpg'}, {reply_to_message_id: ctx.message.message_id});
 	}
 	else if (includesRub(ctx.message.text || ""))
 	{
-		await ctx.reply(`https://picsum.photos/id/${randomInteger(1, 300)}/400/500`);
+		await ctx.telegram.sendAudio(ctx.message.chat.id, {source: 'strigoi.mp3'}, {reply_to_message_id: ctx.message.message_id});
 	}
 }
 async function sendYou(ctx)
 {
-	arrXu.includes(ctx.message.text) ? await ctx.reply('сам иди на хуй') : null;
+	if(arrXu.includes(ctx.message.text))
+	{
+		await ctx.reply('сам иди на хуй', {reply_to_message_id: ctx.message.message_id});
+	}
 }
 let arrXu = ['иди на хуй', 'иди нахуй', 'пошел на хуй', 'пошел нахуй', 'нахуй иди', 'на хуй иди', 'пошёл нахуй', 'пошёл на хуй', 'нахуй пошёл', 'на хуй пошёл'];
 
@@ -58,7 +61,7 @@ function includesRub(text)
 // искать в сообщения слово которое может совпадать с любым словом из массива strigoi убирать пробелы повтрряющиеся буквы и приводить к нижнему регистру
 function includesStrigoi(text)
 {
-	let strigoi = ['strigoi', 'стригой', 'стриг', 'strig', 'стригой']
+	let strigoi = ['strigoi', 'стригой', 'стриг', 'strig', 'стригой', 'sстригой', 'сtригой', 'stригой', 'cTrigoi', 'cтригой', 'стrигой', 'стригoй', 'стрNгой', 'стриrой' ]
 	
 	let strigoiText = text.toLowerCase().replace(/\s/g, "").replace(/(.)\1+/g, "$1");
 	
