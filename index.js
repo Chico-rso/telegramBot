a5f013ccee8d96c86a8405ce3fa0357fa24a2f0f
Amiran <chicobes1@gmail.com>
2022-12-31 16:45:30 +0300

    update v1.2

--- index.js
+++ index.js
@@ -8,12 +8,11 @@ const bot = new Telegraf("5349322670:AAGgL_PuGYb8T6CHxy_ZqY4BlMOnn8WaC64");
 
 const API_KEY = "a1a5763c6ce3ed3ae0df7930f0d187b2";
 
-
 bot.start((ctx) => ctx.reply("Привет!"));
-bot.hears('/random', (ctx) =>
+bot.hears("/random", (ctx) =>
 {
 	let randomId = Math.floor(Math.random() * 300);
-	ctx.reply(`https://picsum.photos/id/${randomId}/400/600/`)
+	ctx.reply(`https://picsum.photos/id/${randomId}/400/600/`);
 });
 bot.command("weather", (ctx) =>
 {
@@ -28,27 +27,37 @@ bot.on("sticker", (ctx) =>
 		ctx.reply("👌");
 	}
 });
-
-bot.on("voice", (ctx) =>
+const strigoi = ["strigoi", "стригой", "стриг", "strig", "стригой", "sстригой", "сtригой", "stригой", "cTrigoi", "cтригой", "стrигой", "стригoй", "стрNгой", "стриrой"];
+const rubai = ['рубай', 'рубай.', 'рубай,', "рубс", "рубенс", "rubai", "rubs", "rubens", "Рубай", "Рубай.", "Рубай,", "Рубс", "Рубенс", "Rubai", "Rubs", "Rubens", "рубчик"];
+function checkRubai(ctx)
 {
-	if (ctx.message.voice.duration > 18)
+	if (rubai.includes(ctx.message.text))
 	{
-		ctx.reply(`${ctx.message.voice.duration} сек слушать это, ${ctx.message.from.first_name} да ну тебя...`);
+		ctx.telegram.sendAudio(ctx.message.chat.id, {source: "strigoi.mp3"}, {reply_to_message_id: ctx.message.message_id});
 	}
-});
+}
 bot.on("message", (ctx) =>
 {
-	hendler(ctx);
 	sendYou(ctx);
+	checkRubai(ctx);
+	const message = ctx.update.message;
+	// check if the message text contains "strigoi"
+	if (message.text && strigoi.some((word) => message.text.toLowerCase().includes(word)))
+	{
+		// send a photo to the user
+		ctx.replyWithPhoto({source: "strigoi.jpg"}, {reply_to_message_id: ctx.message.message_id});
+	}
 });
 
+
 // function to send the New Year's greeting
-function sendMessage() {
-  bot.telegram.sendMessage(315793010, "С новым годом Pediki, люблю вас и обнимаю до хруста! 🎉🎊🎈");
+function sendMessage()
+{
+	bot.telegram.sendMessage(-1001695052259, "С новым годом Pediki, люблю вас и обнимаю до хруста! 🎉🎊🎈");
 }
 
 // schedule the message to be sent at 00:01 on 1.1.23
-const targetDate = new Date('01/01/23 00:01:00');
+const targetDate = new Date("01/01/23 00:01:00");
 const timeUntilTargetDate = targetDate - Date.now();
 setTimeout(sendMessage, timeUntilTargetDate);
 
@@ -74,18 +83,6 @@ async function getWeather(location, ctx)
 	}
 }
 
-async function hendler(ctx)
-{
-	if (includesStrigoi(ctx.message.text))
-	{
-		await ctx.telegram.sendPhoto(ctx.message.chat.id, {source: "strigoi.jpg"}, {reply_to_message_id: ctx.message.message_id});
-	}
-	else if (includesRub(ctx.message.text || ""))
-	{
-		await ctx.telegram.sendAudio(ctx.message.chat.id, {source: "strigoi.mp3"}, {reply_to_message_id: ctx.message.message_id});
-	}
-}
-
 async function sendYou(ctx)
 {
 	if (arrXu.includes(ctx.message.text))
@@ -95,30 +92,6 @@ async function sendYou(ctx)
 }
 
 let arrXu = ["иди на хуй", "иди нахуй", "пошел на хуй", "пошел нахуй", "нахуй иди", "на хуй иди", "пошёл нахуй", "пошёл на хуй", "нахуй пошёл", "на хуй пошёл"];
-
-// искать в сообщении слово "руб" убирать пробелы и приводить к нижнему регистру
-function includesRub(text)
-{
-	return text.toLowerCase().replace(/\s/g, "").includes("руба");
-}
-
-// искать в сообщения слово которое может совпадать с любым словом из массива strigoi убирать пробелы повтрряющиеся буквы и приводить к нижнему регистру
-function includesStrigoi(text)
-{
-	let strigoi = ["strigoi", "стригой", "стриг", "strig", "стригой", "sстригой", "сtригой", "stригой", "cTrigoi", "cтригой", "стrигой", "стригoй", "стрNгой", "стриrой"];
-	
-	let strigoiText = text.toLowerCase().replace(/\s/g, "").replace(/(.)\1+/g, "$1");
-	
-	for (let i = 0; i < strigoi.length; i++)
-	{
-		if (strigoiText.includes(strigoi[i]))
-		{
-			return true;
-		}
-	}
-	
-	return false;
-}
 
 
 bot.launch();
